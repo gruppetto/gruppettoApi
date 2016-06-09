b41e07d495e433c62ab40dee0290527ad6f822b6
Martin Deligny <martin.deligny@adeo.com>
2016-06-09 13:57:03 +0200

    chore: update responses + doc for /index

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,42 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  res.json(
+    {
+      'users': [
+        {
+          'Method': 'GET /users',
+          'Response': [
+            {
+              _id: "string",
+              imageLink: "string",
+              name: "string"
+            }
+          ]
+        },
+        {
+          'Method': 'GET /users/:id',
+          'Response': {
+            _id: "string",
+            imageLink: "string",
+            name: "string"
+          }
+        },
+        {
+          'Method': 'POST /users',
+          'Body': {
+            name: 'string',
+            imageLink: 'string'
+          },
+          'Response': {
+            code: 200,
+            message: 'User created'
+          }
+        }
+      ]
+    }
+  );
   res.status(200).end();
 });
 
--- routes/users.js
+++ routes/users.js
@@ -7,16 +7,34 @@ var router = express.Router();
 mongoose.connect('mongodb://gruppetto:gruppetto@ds021333.mlab.com:21333/gruppetto');
 
 
-router.get('/',function (req, res) {
+router.get('/', function (req, res) {
   User.find(function (err, users) {
-    if (err)
+    if (err){
+      console.log(err);
       res.send(err);
+    }
 
     res.json(users);
   });
 });
 
-router.post('/',function (req, res) {
+router.get('/:id', function (req, res) {
+  User.findById(req.params.id, function (err, user) {
+    if (err){
+      console.log(err);
+      res.json(
+        {
+          message: 'User not found'
+        }
+      );
+      res.status(404).end();
+    }
+
+    res.json(user);
+  });
+});
+
+router.post('/', function (req, res) {
 
   var user = new User();      // create a new instance of the User model
   user.name = req.body.name;
@@ -24,12 +42,14 @@ router.post('/',function (req, res) {
 
   // save the bear and check for errors
   user.save(function (err) {
-    if (err)
+    if (err){
+      console.log(err);
       res.send(err);
+    }
 
     res.json(
       {
-        message: 'Created'
+        message: 'User created'
       }
     );
     res.status(200).end();

1d56d2654d289b2343539d4fe00b52a37e018a90
Martin Deligny <martin.deligny@adeo.com>
2016-06-29 16:28:43 +0200

    fix: update username when logging with fb

--- routes/login.js
+++ routes/login.js
@@ -9,24 +9,24 @@ router.post('/', function (req, res) {
   console.log(req.body);
 
   User.find({
-    email : req.body.email
-  }).exec(function(err, user) {
+    email: req.body.email
+  }).exec(function (err, user) {
     if (err) {
       console.log('error:' + err);
     }
 
-    if(user.length === 0) {
+    if (user.length === 0) {
       console.log('no user found');
 
       var newUser = new User();      // create a new instance of the User model
-      newUser.name = req.body.name;
+      newUser.name = req.body.userName;
       newUser.fbId = req.body.fbId;
       newUser.pictureUrl = req.body.pictureUrl;
       newUser.email = req.body.email;
 
       // save the bear and check for errors
       newUser.save(function (err) {
-        if (err){
+        if (err) {
           console.log(err);
           res.send(err);
         }
@@ -40,8 +40,21 @@ router.post('/', function (req, res) {
       });
     }
     else {
+      var foundUser = user[0];
+
+      foundUser.name = req.body.userName;
+      foundUser.pictureUrl = req.body.pictureUrl;
+
       console.log(user);
-      res.json(user);
+
+      foundUser.save(function (err) {
+        if (err) {
+          console.log(err);
+          res.send(err);
+        }
+        res.send(user)
+      });
+
     }
 
   });

31e916f1cf168a1640e65418bc87cdc99b7dddef
Martin Deligny <martin.deligny@adeo.com>
2016-06-29 17:12:32 +0200

    chore: update doc

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,20 @@ router.get('/', function (req, res, next) {
     {
       'users': [
         {
+          'Method' : 'POST /login',
+          'body': {
+            'email': 'String (required)',
+            'name':'String (optionnal)',
+            'pictureUrl': 'String (optionnal)',
+            'fbId':'String (optionnal)'
+          }
+        },
+        {
           'Method': 'GET /users',
           'Response': [
             {
               _id: "string",
-              imageLink: "string",
+              pictureUrl: "string",
               name: "string"
             }
           ]
@@ -20,20 +29,39 @@ router.get('/', function (req, res, next) {
           'Method': 'GET /users/:id',
           'Response': {
             _id: "string",
-            imageLink: "string",
+            pictureUrl: "string",
             name: "string"
           }
         },
         {
-          'Method': 'POST /users',
-          'Body': {
-            name: 'string',
-            imageLink: 'string'
-          },
-          'Response': {
-            code: 200,
-            message: 'User created'
+          'Method' : 'POST /users/:id/groups',
+          'body': {
+            'name':'String (required)'
           }
+        },
+        {
+          'Method': 'GET /users/:id/groups',
+          'Response': [
+            {
+              _id: 'String',
+              name: 'String',
+              members: '[String]',
+              admin: '[String]'
+            }
+          ]
+        }
+      ],
+      'groups' : [
+        {
+          'Method' : 'GET /groups',
+          'Response': [
+            {
+              _id: 'String',
+              name: 'String',
+              members: '[String]',
+              admin: '[String]'
+            }
+          ]
         }
       ]
     }

2e88ef4e895f5226fdb4d998654b4872fa336266
Martin Deligny <martin.deligny@adeo.com>
2016-08-19 17:44:56 +0200

    chore: require token for group manipulation

--- routes/groups.js
+++ routes/groups.js
@@ -4,6 +4,10 @@ var User = require('../models/user');
 
 var router = express.Router();
 
+var jwt = require('jwt-simple');
+var config = require('../config/database');
+var passport = require('passport');
+
 // get all
 
 router.get('/', function (req, res) {
@@ -26,104 +30,195 @@ router.get('/', function (req, res) {
 
 // update group
 
-router.put('/:id', function (req, res) {
+router.put('/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+  var token = getToken(req.headers);
 
-  Group.findById(req.params.id, function (err, group) {
-    if (err)
-      res.send(err);
+  if (token) {
+    var decoded = jwt.decode(token, config.secret);
 
-    group.name = req.body.name;
+    User.findOne({
+      email: decoded.email
+    }, function (err, user) {
+      if (err) throw err;
 
-    group.save(function (err) {
-      if (err)
-        res.send(err);
+      if (!user) {
 
-      res.json({success: true, message: 'Group updated'});
+        return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+      }
+      else {
+        Group.findById(req.params.id, function (err, group) {
+          if (err)
+            res.send(err);
+
+          group.name = req.body.name;
+
+          group.save(function (err) {
+            if (err)
+              res.send(err);
+
+            res.json({success: true, message: 'Group updated'});
+          });
+        });
+      }
+
     });
-  });
+  }
+  else {
 
+    return res.status(403).send({success: false, msg: 'No token provided.'});
+  }
 });
 
 // delete group by id
 
-router.delete('/:id', function (req, res) {
-  Group.findByIdAndRemove(req.params.id, function (err) {
-    if (err)
-      res.send(err);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+  var token = getToken(req.headers);
 
-    res.json({success: true, message: 'Group deleted'});
-  });
+  if (token) {
+    var decoded = jwt.decode(token, config.secret);
+
+    User.findOne({
+      email: decoded.email
+    }, function (err, user) {
+      if (err) throw err;
+
+      if (!user) {
+
+        return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+      }
+      else {
+        Group.findByIdAndRemove(req.params.id, function (err) {
+          if (err)
+            res.send(err);
+
+          res.json({success: true, message: 'Group deleted'});
+        });
+      }
+
+    });
+  }
+  else {
+
+    return res.status(403).send({success: false, msg: 'No token provided.'});
+  }
 });
 
 // Leave group
 
-router.delete('/:id/members/:memberId', function (req, res) {
-  Group.findById(req.params.id, function (err, group) {
-    if (err) {
-      res.json(
-        {
-          message: 'Group not found'
-        }
-      );
-      res.status(404).end();
-    }
+router.delete('/:id/members/:memberId', passport.authenticate('jwt', {session: false}), function (req, res) {
+  var token = getToken(req.headers);
 
-    User.findById(req.params.memberId, function (err, user) {
-      if (err) {
-        res.json(
-          {
-            message: 'User not found'
+  if (token) {
+    var decoded = jwt.decode(token, config.secret);
+
+    User.findOne({
+      email: decoded.email
+    }, function (err, user) {
+      if (err) throw err;
+
+      if (!user) {
+
+        return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+      }
+      else {
+        Group.findById(req.params.id, function (err, group) {
+          if (err) {
+            res.json(
+              {
+                message: 'Group not found'
+              }
+            );
+            res.status(404).end();
           }
-        );
+
+          User.findById(req.params.memberId, function (err, user) {
+            if (err) {
+              res.json(
+                {
+                  message: 'User not found'
+                }
+              );
+            }
+
+            group.members.remove(user._id);
+            group.admin.remove(user._id);
+
+            group.save(function (err) {
+              if (err)
+                res.send(err);
+
+              res.json({success: true, message: 'group left'});
+            });
+          });
+
+        });
       }
 
-      group.members.remove(user._id);
-      group.admin.remove(user._id);
-
-      group.save(function (err) {
-        if (err)
-          res.send(err);
-
-        res.json({success: true, message: 'group left'});
-      });
     });
+  }
+  else {
 
-  });
+    return res.status(403).send({success: false, msg: 'No token provided.'});
+  }
 });
 
 // Join group
 
-router.post('/:id/members', function (req, res) {
-  Group.findById(req.params.id, function (err, group) {
-    if (err) {
-      res.json(
-        {
-          message: 'Group not found'
-        }
-      );
-      res.status(404).end();
-    }
+router.post('/:id/members', passport.authenticate('jwt', {session: false}), function (req, res) {
+  var token = getToken(req.headers);
 
-    User.findById(req.body.memberId, function (err, user) {
-      if (err) {
-        res.json(
-          {
-            message: 'User not found'
+  if (token) {
+    var decoded = jwt.decode(token, config.secret);
+
+    User.findOne({
+      email: decoded.email
+    }, function (err, user) {
+      if (err) throw err;
+
+      if (!user) {
+
+        return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+      }
+      else {
+
+        Group.findById(req.params.id, function (err, group) {
+          if (err) {
+            res.json(
+              {
+                message: 'Group not found'
+              }
+            );
+            res.status(404).end();
           }
-        );
+
+          User.findById(req.body.memberId, function (err, user) {
+            if (err) {
+              res.json(
+                {
+                  message: 'User not found'
+                }
+              );
+            }
+
+            group.members.push(user._id);
+
+            group.save(function (err) {
+              if (err)
+                res.send(err);
+
+              res.json({success: true, message: 'group updated'});
+            });
+          });
+
+        });
       }
 
-      group.members.push(user._id);
-
-      group.save(function (err) {
-        if (err)
-          res.send(err);
-
-        res.json({success: true, message: 'group updated'});
-      });
     });
+  }
+  else {
 
-  });
+    return res.status(403).send({success: false, msg: 'No token provided.'});
+  }
 });
 
 
@@ -147,5 +242,19 @@ router.get('/:id', function (req, res) {
       res.json(group);
     });
 });
+
+
+getToken = function (headers) {
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,9 @@ var Group = require('../models/group');
 var mongoose = require('mongoose');
 
 var router = express.Router();
-
+var jwt = require('jwt-simple');
+var config = require('../config/database');
+var passport = require('passport');
 
 /*
  *
@@ -176,13 +178,51 @@ router.put('/:id', function (req, res) {
 
 // delete user
 
-router.delete('/:id', function (req, res) {
-  User.findByIdAndRemove(req.params.id, function (err) {
-    if (err)
-      res.send(err);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), function (req, res) {
+  var token = getToken(req.headers);
 
-    res.json({success: true, message: 'User deleted'});
-  });
+  if (token) {
+    var decoded = jwt.decode(token, config.secret);
+
+    User.findOne({
+      email: decoded.email
+    }, function (err, user) {
+      if (err) throw err;
+
+      if (!user) {
+
+        return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+      }
+      else {
+
+        User.findByIdAndRemove(req.params.id, function (err) {
+          if (err)
+            res.send(err);
+
+          res.json({success: true, message: 'User deleted'});
+        });
+      }
+
+    });
+  }
+  else {
+
+    return res.status(403).send({success: false, msg: 'No token provided.'});
+  }
 });
+
+
+getToken = function (headers) {
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
 
 module.exports = router;

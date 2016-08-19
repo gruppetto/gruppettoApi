b17cced1b1a53393d04106c288230d2ed1edee1a
Martin Deligny <martin.deligny@adeo.com>
2016-08-19 13:20:14 +0200

    feat: update group, join group, delete group

--- routes/groups.js
+++ routes/groups.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var Group = require('../models/group');
+var User = require('../models/user');
 
 var router = express.Router();
+
+// get all
 
 router.get('/', function (req, res) {
   Group
@@ -21,13 +24,120 @@ router.get('/', function (req, res) {
     });
 });
 
+// update group
+
+router.put('/:id', function (req, res) {
+
+  Group.findById(req.params.id, function (err, group) {
+    if (err)
+      res.send(err);
+
+    group.name = req.body.name;
+
+    group.save(function (err) {
+      if (err)
+        res.send(err);
+
+      res.json({success: true, message: 'Group updated'});
+    });
+  });
+
+});
+
+// delete group by id
+
+router.delete('/:id', function (req, res) {
+  Group.findByIdAndRemove(req.params.id, function (err) {
+    if (err)
+      res.send(err);
+
+    res.json({success: true, message: 'Group deleted'});
+  });
+});
+
+// Leave group
+
+router.delete('/:id/members/:memberId', function (req, res) {
+  Group.findById(req.params.id, function (err, group) {
+    if (err) {
+      res.json(
+        {
+          message: 'Group not found'
+        }
+      );
+      res.status(404).end();
+    }
+
+    User.findById(req.params.memberId, function (err, user) {
+      if (err) {
+        res.json(
+          {
+            message: 'User not found'
+          }
+        );
+      }
+
+      group.members = group.members.filter(function (memberId) {
+        return memberId !== user._id;
+      });
+
+      group.save(function (err) {
+        if (err)
+          res.send(err);
+
+        res.json({success: true, message: 'group left'});
+      });
+    });
+
+  });
+});
+
+// Join group
+
+router.post('/:id/members', function (req, res) {
+  Group.findById(req.params.id, function (err, group) {
+    if (err) {
+      res.json(
+        {
+          message: 'Group not found'
+        }
+      );
+      res.status(404).end();
+    }
+
+    User.findById(req.body.memberId, function (err, user) {
+      if (err) {
+        res.json(
+          {
+            message: 'User not found'
+          }
+        );
+      }
+
+      group.members.push(user._id);
+
+      group.save(function (err) {
+        if (err)
+          res.send(err);
+
+        res.json({success: true, message: 'group updated'});
+      });
+    });
+
+  });
+});
+
+
+// Get by id
+
+
 router.get('/:id', function (req, res) {
   Group
     .findById(req.params.id)
     .populate(['admin', 'members'])
     .exec(function (err, group) {
       if (err) {
-        console.log(err);
+
         res.json(
           {
             message: 'Group not found'
